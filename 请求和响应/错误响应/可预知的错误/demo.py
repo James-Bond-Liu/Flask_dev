aabd0c1dfedd9922073c7d26816844143b38c4d8
liufei <779542742@qq.com>
2021-11-09 20:15:16 +0800

    异常处理

---
+++ 请求和响应/错误响应/可预知的错误/demo.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2021/11/9 19:53
+@Auth ： Liu Fei
+@File ：demo.py
+@IDE ：PyCharm
+"""
+from flask import Flask, request, abort
+
+# 实例化app
+app = Flask(import_name=__name__)
+
+@app.route('/login', methods=["GET","POST"])
+def login():
+
+    if request.args.get('user_name') != 'libai' and request.args.get('user_pwd') != '123':
+        abort(404)
+
+    return "login sucess"
+
+# 定义错误处理的方法
+@app.errorhandler(404)
+def handle_404_error(err):
+    """自定义的处理错误方法"""
+    # 这个函数的返回值会是前端用户看到的最终结果
+    return "出现了404错误， 错误信息：%s" % err
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- 请求和响应/错误响应/可预知的错误/抛出json格式的异常.py
+++ 请求和响应/错误响应/可预知的错误/抛出json格式的异常.py
@@ -1,22 +1,29 @@
 import json
 
-from flask import Flask, redirect, request, url_for, render_template, make_response, abort
+from flask import Flask, redirect, request, url_for, render_template, make_response, abort, jsonify
 
 app = Flask(__name__)
+
 
 class UserError(Exception):  # 自定义一个错误类型，继承错误的基类
     pass
 
+
 # 当全局出现UserError的错误类型如何处理
 @app.errorhandler(UserError)  # 全局处理UserError错误类型
 def server_error(error):
-    return render_template('user_error_401.html',error=error), 401
+    # return render_template('user_error_401.html',error=error), 401
+    res = jsonify({"msg": "error:username is None"})
+    res.status_code = 401
+    return res
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.args.get('username') is None:
-        raise UserError()  #  抛出自定义的错误类型
+        raise UserError()  # 抛出自定义的错误类型和错误内容提示
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
