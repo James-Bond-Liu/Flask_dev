09a3959fe9b3ab782f6b4d8b298f911dc44042df
James Bond Liu <779542742@qq.com>
2021-11-11 16:36:00 +0800

    11

--- 模板引擎/one/app.py
+++ 模板引擎/one/app.py
@@ -14,7 +14,9 @@ def index():
     flash('欢迎来到首页')  # 用flash来实现消息闪现，注意必须配置app.config['SECRET_KEY']配置项
 
     # render_template中的**context，上下文管理器。我们可以在后端render_template处添加关键字参数，然后在前端直接获取。
-    return render_template('index.html', p = projects, title = '模板渲染')
+    # 渲染模版时有两种传递参数的方式：用 var='value' 传递一个参数；使用字典组织多个参数，并且加两个**号转换成关键字参数传入。
+    return render_template('index.html', **{"p":projects, "title":'模板渲染'})  # return render_template('index.html', p = projects, title = '模板渲染')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
